3c3984e779b8aa20fca0970e88112023236557c5
r_new <robert.newman@alumni.ubc.ca>
2021-08-17 10:13:07 -0700

    json backend

--- src/App.js
+++ src/App.js
@@ -1,50 +1,80 @@
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import AddTask from './components/AddTask'
 
 function App() {
 
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {id: 1,
-    text: "Doctor's Appointment",
-    day: '2000-05-07',
-    reminder: true},
-    {id: 2, 
-    text: "Meeting at School", 
-    day: '2000-05-08', 
-    reminder: true},
-    {id: 3,
-    text: "Grocery Shopping",
-    day: '2000-05-09',
-    reminder: false},
-  ])
+  const [tasks, setTasks] = useState([])
 
-  // FILTER FOR THE TASKS WHOSE ID IS NOT EQUAL TO ID! SAVE THEM INTO the state as a NEW OBJECT
-  // Filter for the tasks whose ID is not equal to id! (i.e. filter out the task whose ID IS equal to ID!)
+  useEffect(() => {
 
-  // MAP AND FILTER RETURN ARRAYS! INSIDE ARRAY, TASKS ARE "OBJECTS!" ALLOWING US TO RETURN AN ARRAY OF OBJECTS WHEN WE CALL FILTER/MAP FROM SET, TASKS. PASSING IN (TASK), TASK IS AN OBJECT. 
-  // ARRAY OF OBJECTS!
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  } 
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  } 
+
 
   const toggleAddTask = () => {
     setShowAddTask(!showAddTask)
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
-  // So not just return an object where {... but need to include variable name (i.e. the name of the variable storing the object). So ...task is "everything in task except what comes after comma"}
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch (`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers:{ 'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
   }
 
-  const addTask = (task) => {
+  const addTask = async (task) => {
     const id = Math.floor(Math.random() * 100000) + 1
     const newTask = {id, ...task}
+
+    await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(newTask)
+    })
     setTasks([...tasks, newTask])
   }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,8 +2,8 @@ import { FaTrash } from 'react-icons/fa'
 const Task = ({task, onDelete, onToggle}) => {
 
     return (
-        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
-            <h3>{`${task.text}`} <FaTrash style= {{color: 'grey'}} onClick={() => onDelete(task.id)}></FaTrash></h3>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} style = {{cursor: 'default'}} onDoubleClick={() => onToggle(task.id)}>
+            <h3>{`${task.text}`} <FaTrash style= {{color: 'grey', cursor: 'pointer'}} onClick={() => onDelete(task.id)}></FaTrash></h3>
             <p>{task.day}</p>
         </div>
     )
